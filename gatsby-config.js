b000cdbb90aa355b3844f6366e2715091cf49955
Peter Chapman <petechapman@protonmail.com>
2021-01-24 16:19:41 -0500

    removed pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,18 +59,17 @@ module.exports = {
     // },
     `gatsby-plugin-feed`,
     `gatsby-plugin-sass`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `Gatsby Starter Blog`,
+    //     short_name: `GatsbyJS`,
+    //     start_url: `/`,
+    //     background_color: `#ffffff`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //   },
+    // },
     `gatsby-plugin-react-helmet`,
     'gatsby-plugin-dark-mode',
     // this (optional) plugin enables Progressive Web App + Offline functionality
