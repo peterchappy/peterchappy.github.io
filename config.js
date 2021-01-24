aae9d79cd20f1536064fc8e7a7d6482e9c3ef3f9
Peter Chapman <petechapman@protonmail.com>
2021-01-24 14:38:17 -0500

    prefix

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 module.exports = {
   url: `https://chapman.codes`,
   description: `my corner of the internet.`,
-  pathPrefix: 'peterchappy.github.io',
+  pathPrefix: '/peterchappy.github.io',
   title: `chapman.codes`,
   disqusShortname: '',
   postsPerPage: 15,
